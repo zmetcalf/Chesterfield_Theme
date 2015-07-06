80a9756ee13ad79a75b23a91085793f22472345c
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-07-05 20:09:20 -0600

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).foundation();
+
+$(function() {
+  var slides = [];
+
+  $.each($('.background_images'), function (index, image) {
+    slides.push({ src: $(image).attr('data-image') });
+  });
+
+  $('body').vegas({
+    slides: slides
+  });
+});
